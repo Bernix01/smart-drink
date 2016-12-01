7da1a24a3f682df645a87fa6d0ea16541de1ecd0
bernix01 <gbernal096@gmail.com>
2016-12-01 13:18:04 -0500

    final

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,10 +4,10 @@ void setup() {
 
 void loop() {
   
-  int adc_MQ = analogRead(A0); //Lemos la salida analógica  del MQ
+  int adc_MQ = analogRead(A6); //Lemos la salida analógica  del MQ
   float voltaje = adc_MQ * (5.0 / 1023.0); //Convertimos la lectura en un valor de voltaje
   float Rs=1000*((5-voltaje)/voltaje);  //Calculamos Rs con un RL de 1k
   double alcohol=0.4091*pow(Rs/5463, -1.497); // calculamos la concentración  de alcohol con la ecuación obtenida.
-  Serial.print(alcohol+"mg/L");
-  delay(100);
+  Serial.print(String(alcohol)+"mg/L|");
+  delay(300);
 }
